9108e739db4ac87da324e7324df17479cd2c12d8
harlan <hello@harlankellaway.com>
2018-04-25 23:47:42 -0400

    Updated tests

---
+++ Tests/FakeFileSystem.swift
@@ -0,0 +1,50 @@
+//
+//  FakeFileSystem.swift
+//  Purse
+//
+//  Created by Harlan Kellaway on 4/25/18.
+//  Copyright © 2018 Harlan Kellaway. All rights reserved.
+//
+
+import Foundation
+
+import XCTest
+@testable import Purse
+
+final class FakeFileSystem: FileSystem {
+    
+    var appGroupURL: URL? = nil
+    var applicationSupportURL: URL? = nil
+    var cachesURL: URL? = nil
+    var documentsURL: URL? = nil
+    var tempURL: URL? = nil
+    var userHomeURL: URL? = nil
+    
+    init() { }
+    
+    func appGroupContainerURL(appGroupName: String) -> URL? {
+        return appGroupURL
+    }
+    
+    func temporaryDirectoryURL() -> URL? {
+        return tempURL
+    }
+    
+    func userHomeURL(toDirectory directory: Purse.Directory) -> URL? {
+        switch directory {
+        case .applicationSupport:
+            return applicationSupportURL
+        case .caches:
+            return cachesURL
+        case .documents:
+            return documentsURL
+        default:
+            return nil
+        }
+    }
+    
+    func createDirectoryIfNeeded(at url: URL) throws {
+        return
+    }
+    
+}
--- Tests/URLConstructorTests.swift
+++ Tests/URLConstructorTests.swift
@@ -14,11 +14,25 @@ import XCTest
 class URLConstructorTests: XCTestCase {
     
     var sut: URLConstructor!
+    var fakeAppGroupURL = "someDirectory/AppGroup"
+    var fakeApplicationSupportURL = "someDirectory/Application"
+    var fakeCachesURL = "someDirectory/Caches"
+    var fakeDocumentsURL = "someDirectory/Documents"
+    var fakeTempURL = "someDirectory/Temp"
+    var fakeUserHomeURL = "someDirectory/Home"
     
     override func setUp() {
         super.setUp()
         
-        sut = PurseURLConstructor()
+        let fakeFileSystem = FakeFileSystem()
+        fakeFileSystem.appGroupURL = URL(string: fakeAppGroupURL)
+        fakeFileSystem.applicationSupportURL = URL(string: fakeApplicationSupportURL)
+        fakeFileSystem.cachesURL = URL(string: fakeCachesURL)
+        fakeFileSystem.documentsURL = URL(string: fakeDocumentsURL)
+        fakeFileSystem.tempURL = URL(string: fakeTempURL)
+        fakeFileSystem.userHomeURL = URL(string: fakeUserHomeURL)
+        
+        sut = PurseURLConstructor(fileSystem: fakeFileSystem)
     }
     
     override func tearDown() {
@@ -30,7 +44,7 @@ class URLConstructorTests: XCTestCase {
     func testCreateURL_shouldReturnCorrectURL_forApplicationSupport() {
         let fileName: FileName = "test"
         let actual = try! sut.createURL(fileName: fileName, in: .applicationSupport)
-        let expected = URL(string: "file:///Users/harlan/Library/Developer/CoreSimulator/Devices/B5DEFB5F-4E06-4146-8AA5-4A7690510E9F/data/Library/Application%20Support/test")!
+        let expected = URL(string: "file://\(fakeApplicationSupportURL)/test")!
         
         XCTAssertEqual(actual, expected)
     }
@@ -38,7 +52,7 @@ class URLConstructorTests: XCTestCase {
     func testCreateURL_shouldReturnCorrectURL_forCaches() {
         let fileName: FileName = "test"
         let actual = try! sut.createURL(fileName: fileName, in: .caches)
-        let expected = URL(string: "file:///Users/harlan/Library/Developer/CoreSimulator/Devices/B5DEFB5F-4E06-4146-8AA5-4A7690510E9F/data/Library/Caches/test")!
+        let expected = URL(string: "file://\(fakeCachesURL)/test")!
         
         XCTAssertEqual(actual, expected)
     }
@@ -46,24 +60,24 @@ class URLConstructorTests: XCTestCase {
     func testCreateURL_shouldReturnCorrectURL_forDocuments() {
         let fileName: FileName = "test"
         let actual = try! sut.createURL(fileName: fileName, in: .documents)
-        let expected = URL(string: "file:///Users/harlan/Library/Developer/CoreSimulator/Devices/B5DEFB5F-4E06-4146-8AA5-4A7690510E9F/data/Documents/test")!
+        let expected = URL(string: "file://\(fakeDocumentsURL)/test")!
         
         XCTAssertEqual(actual, expected)
     }
 
-//    func testCreateURL_shouldReturnCorrectURL_forSharedContainer() {
-//        let fileName: FileName = "test"
-//        let appGroupName = "name"
-//        let actual = try! sut.createURL(fileName: fileName, in: .sharedContainer(appGroupName: appGroupName))
-//        let expected = URL(string: "https://google.com")!
-//
-//        XCTAssertEqual(actual, expected)
-//    }
+    func testCreateURL_shouldReturnCorrectURL_forSharedContainer() {
+        let fileName: FileName = "test"
+        let appGroupName = "name"
+        let actual = try! sut.createURL(fileName: fileName, in: .sharedContainer(appGroupName: appGroupName))
+        let expected = URL(string: "file://\(fakeAppGroupURL)/test")!
+
+        XCTAssertEqual(actual, expected)
+    }
     
     func testCreateURL_shouldReturnCorrectURL_forTemporary() {
         let fileName: FileName = "test"
         let actual = try! sut.createURL(fileName: fileName, in: .temporary)
-        let expected = URL(string: "file:///Users/harlan/Library/Developer/CoreSimulator/Devices/B5DEFB5F-4E06-4146-8AA5-4A7690510E9F/data/tmp/test")!
+        let expected = URL(string: "file://\(fakeTempURL)/test")!
         
         XCTAssertEqual(actual, expected)
     }
